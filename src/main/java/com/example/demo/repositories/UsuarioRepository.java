c67eff4cb9b1501059c64b85139edff52a4cc1e4
Mauricio Estupiñan <mauricio_estupinan@yahoo.com>
2026-03-10 20:31:03 -0500

    consulta Data lombok

--- src/main/java/com/example/demo/repositories/UsuarioRepository.java
+++ src/main/java/com/example/demo/repositories/UsuarioRepository.java
@@ -9,9 +9,11 @@ import com.example.demo.models.Usuario;
 
 public interface UsuarioRepository extends MongoRepository<Usuario, String> {
 
-    Optional<Usuario> findByDocNumero(String docnum);
-
     // consulta usuario por numero documento
     @Query("{'doc.numero': ?0}")
     Optional<Usuario> findByDocNum(String docnum);
+
+    // consulta usuario por numero documento usando método de consulta derivada
+    Optional<Usuario> findByDocNumero(String numero);
+
 }
